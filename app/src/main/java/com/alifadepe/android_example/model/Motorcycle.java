87a900049c066757391c8dfedb6bab797a8969f0
I Made Akira Ivandio Agusta <ivandioakira@gmail.com>
2020-11-15 18:38:05 +0800

    Added item for motorcycle

--- app/src/main/java/com/alifadepe/android_example/model/Motorcycle.java
+++ app/src/main/java/com/alifadepe/android_example/model/Motorcycle.java
@@ -1,4 +1,47 @@
 package com.alifadepe.android_example.model;
 
 public class Motorcycle {
+    private int motorcycleId;
+    private String brand;
+    private String plateNumber;
+    private int userId;
+
+    public Motorcycle(int motorcycleId, String brand, String plateNumber, int userId) {
+        this.motorcycleId = motorcycleId;
+        this.brand = brand;
+        this.plateNumber = plateNumber;
+        this.userId = userId;
+    }
+
+    public int getMotorcycleId() {
+        return motorcycleId;
+    }
+
+    public void setMotorcycleId(int motorcycleId) {
+        this.motorcycleId = motorcycleId;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 }
