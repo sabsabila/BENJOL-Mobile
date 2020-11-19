9437b9352983a1a149b43ef37f9547ee4c8aed18
I Made Akira Ivandio Agusta <ivandioakira@gmail.com>
2020-11-19 21:14:21 +0800

    Pickup Activity

---
+++ app/src/main/java/com/alifadepe/android_example/view/PickupActivity.java
@@ -0,0 +1,70 @@
+package com.alifadepe.android_example.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.alifadepe.android_example.contract.PickupContract;
+import com.alifadepe.android_example.databinding.ActivityPickupBinding;
+import com.alifadepe.android_example.presenter.PickupPresenter;
+
+import java.util.ArrayList;
+
+public class PickupActivity extends AppCompatActivity implements PickupContract.View, View.OnClickListener {
+    private PickupContract.Presenter presenter;
+    private ActivityPickupBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityPickupBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.baseLayout.backButton.setEnabled(true);
+
+        presenter = new PickupPresenter(this);
+        initView();
+    }
+
+    private void initView(){
+        binding.baseLayout.pageTitle.setText("Pick Up");
+        binding.baseLayout.backButton.setOnClickListener(this);
+        binding.confirmButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void startLoading() {
+        binding.progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void endLoading() {
+        binding.progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void sendLocation(ArrayList<String> location) {
+        finish();
+
+        Intent returnIntent = new Intent(this, LoginActivity.class); //login ini gantien BookingActivity kalo udah ada
+        returnIntent.putStringArrayListExtra("location", location);
+        startActivity(returnIntent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == binding.confirmButton.getId())
+            onButtonPickupClick();
+        if(v.getId() == binding.baseLayout.backButton.getId())
+            onBackPressed();
+    }
+
+    public void onButtonPickupClick(){
+        ArrayList<String> location = new ArrayList<String>();
+        location.add(binding.pickupLocation.getText().toString());
+        location.add(binding.deliveryLocation.getText().toString());
+
+        presenter.inputLocation(location);
+    }
+}
